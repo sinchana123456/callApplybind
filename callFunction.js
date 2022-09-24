6f4ae69780a7fda9a12a7c98eeefb4ac150e0b13
sinchana123456 <sinchanal543@gmail.com>
2022-09-24 13:30:06 +0530

    call

--- callFunction.js
+++ callFunction.js
@@ -1,9 +1,11 @@
 // JavaScript source code
 var obj = { num: 3 };
+var obj2 = { num: 2 };
 
-var addToThis = function (a) {
-    return this.num + a;
+var addToThis = function (a,b,c) {
+    return this.num + a+b+c;
 
 };
-
-console.log(addToThis.call(obj, 4));
+var arr = [1, 2, 3];
+//console.log(addToThis.call(obj, 4, 5, 6));
+console.log(addToThis.apply(obj2, arr));
